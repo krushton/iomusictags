15410a7c3b33cee5d056e14f27566b5db583367a
krushton <kjrushton@gmail.com>
2012-11-12 20:57:16 -0800

    re-wrote to use giant static json file

--- js/force.js
+++ js/force.js
@@ -1,95 +1,42 @@
-// This commented out nodes works, but has been replaced with the JSON data. I'm leaving it here for reference on how it looks.
-// var nodes = [{"name":"Rock","freq":100},{"name":"Heavy Metal","freq":50},{"name":"Disco","freq":40},{"name":"French House","freq":10}];
+var width = 1200,
+    height = 1000;
 
-// This links is currently being used. It's fake data, since I didn't make an algorithm for calculating similarity yet.
-var links = [{"source":1,"target":0,"value":70},{"source":2,"target":0,"value":30},{"source":3,"target":0,"value":20},{"source":2,"target":1,"value":20},{"source":3,"target":1,"value":10},{"source":3,"target":2,"value":80}];
-
-// These are for creating nodes. nodeNames and nodeFreqs are combined later to form nodes.
-var nodes = [];
-var nodeNames = [];
-var nodeFreqs = [];
-
-// Get similar tags for Pop tag
-$.getJSON("http://ws.audioscrobbler.com/2.0/?method=tag.getsimilar&tag=pop&api_key=1d4e90a59a76ae5489f0a080f0da6979&format=json", function(data){
-  console.log(data);
-
-  // For each similar tag, append it to nodeNames
-  $.each(data.similartags.tag, function(i,tag){
-      var nodeName = tag.name;
-      nodeNames.push(nodeName);
-
-      // Get the popularity of each similar tag and append it to nodeFreqs
-      $.getJSON("http://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag="+tag.name+"&api_key=1d4e90a59a76ae5489f0a080f0da6979&format=json", function(taginfo){
-        nodeFreq = taginfo.tag.taggings/10000; // adjust for radius
-        nodeFreqs.push(nodeFreq);
-
-        // IMPORTANT
-        // I could not figure out how to run the later stuff after the JSONs finished, so I kind of did a hack job here.
-        //After the 4th tag frequency is retrieved, all the JSONs are done. At this point, run the later function.
-        if (nodeFreqs.length == 4){
-          laterFunction();
-        }
-      });
-
-      // Only get the first 4 similar tags to Pop
-      if ( i == 3 ) return false;
-    });
-});
-
-function laterFunction(){
-
-  // Combine the names and frequencies and append them to nodes
-  for (var i = 0; i < nodeNames.length; i++){
-    var node = {"name":nodeNames[i],"freq":nodeFreqs[i]};
-    nodes.push(node);
-  }
-
-  // Just checking stuff
-  console.log(nodeNames);
-  console.log(nodeFreqs);
-  console.log(nodes);
-  console.log(links);
-
-
-  var width = 1200,
-      height = 700;
-
-  var color = d3.scale.category20();
-
-  var force = d3.layout.force()
-      .charge(-120)
-       force.linkDistance(function(link) {
-          return 3 * (100 - link.value); //link.value measures the strength of the correllation, with higher values being a higher "similarity score"
+var force = d3.layout.force()
+    .charge(-20)
+    .linkDistance(function(link) {
+          return 8 * (100 - link.value); //link.value measures the strength of the correllation, with higher values being a higher "similarity score"
        })
-      .size([width, height]);
+    .friction(.1)
+    .gravity(.5)
+    .size([width, height]);
 
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width)
-      .attr("height", height);
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
+d3.json("results.json", function(json) {
   force
-      .nodes(nodes)
-      .links(links)
+      .nodes(json.nodes)
+      .links(json.links)
       .start();
 
-  var link = svg.selectAll("line.link")         //code for links with text adapted from https://groups.google.com/forum/?fromgroups=#!topic/d3-js/GR4FeV85drg
-      .data(links)
+   var link = svg.selectAll("line.link")         //code for links with text adapted from https://groups.google.com/forum/?fromgroups=#!topic/d3-js/GR4FeV85drg
+      .data(json.links)
       .enter().append("line")
       .attr("class", "link")
       .style("stroke-width", 0) //  no stroke renders bubbles in space
       .text("test");
 
-  var node = svg.selectAll("g.node") 
-      .data(nodes) 
+   var node = svg.selectAll("g.node") 
+      .data(json.nodes) 
       .enter().append("svg:g") 
       .attr("class", "node"); 
 
     node.append("svg:circle") 
       .attr("r", function(d) { 
-        console.log(d.freq);
-        return d.freq; }) 
-      .style("fill", function() { return getRandomColor()}) 
-      .call(force.drag); 
+           return Math.floor(d.freq/3); }) 
+      .style("fill", function() { return getRandomColor()});
+     // .call(force.drag); 
 
     node.append("svg:text") 
       .style("pointer-events", "none") 
@@ -105,6 +52,10 @@ function laterFunction(){
       .style("pointer-events", "none") 
       .text(function(d) { return d.name; }); 
 
+    node.on("click", function(d) {
+        handleClick(d);
+    });
+
     force.on("tick", function() { 
 
     link.attr("x1", function(d) { return d.source.x; }) 
@@ -115,7 +66,10 @@ function laterFunction(){
     node.attr("transform", function(d) { return "translate(" + d.x + 
   "," + d.y + ")"; }); 
 
-    }); 
+    });
+
+});
+
 
 
     function getRandomColor() {
@@ -126,6 +80,8 @@ function laterFunction(){
         }
     return color;
     }
-}
-    
-      
+
+    function handleClick(d) {
+      alert('click handler called: ' + d.name)
+    }
+
---
+++ process2.py
@@ -0,0 +1,65 @@
+import json, math, urllib, urllib2
+from collections import OrderedDict
+
+
+nodes = []
+links = []
+
+def call_api(url, params):
+    data = urllib.urlencode(params)
+    req = urllib2.Request(url, data)
+    # Returns a Python dict of the JSON
+    return json.loads(urllib2.urlopen(req).read())
+
+def get_similar_tags(tag):
+	tags = []
+	params = {"method" : "tag.getsimilar", "format" : "json", "tag" : tag, "api_key" : api_key}
+	similarity_data = call_api(url, params)
+	return similarity_data['similartags']['tag']
+
+def find_index(list, name):
+	for i in range(0, len(list)):
+		if (list[i]['name'] == name):
+			return i
+		return -1
+
+def get_sim_score(aTags, bTags):
+	simscore = 0
+	for a in aTags:
+		for b in bTags:
+			if a['name'] == b['name']:
+				simscore = simscore + 2
+
+	return simscore
+
+def calculate_similarity(startPoint):
+     if startPoint == len(nodes)-1:
+         return
+     else:
+     	node = nodes[startPoint]
+     	atags = get_similar_tags(node['name'])
+        for i in range(startPoint+1,len(nodes)):
+         	btags = get_similar_tags(nodes[i]['name'])
+         	similarity = get_sim_score(atags, btags)
+         	links.append({"source": startPoint, "target": i, "value": similarity })
+        calculate_similarity(startPoint+1)
+
+
+url = "http://ws.audioscrobbler.com/2.0/?"
+api_key = "392f7fd530902126a2bc75e9acf5adad"
+
+f = open('toptags.json')
+d = json.load(f)
+
+index = 1
+for tag in d['toptags']['tag']:
+	if index <= 100:
+		nodes.append({"name" : tag['name'], "freq": int( int(tag["count"])/10000)})
+		index = index + 1
+w = open('results.json', 'w')
+
+calculate_similarity(0)
+
+
+complete = { "nodes": nodes, "links": links}
+w.write(json.dumps(complete))
