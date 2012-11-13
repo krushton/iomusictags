bf5ff0e6ee008ea200075181efafdbd33ee6240e
Peter Nguyen <ptt.nguyen@gmail.com>
2012-11-13 02:28:59 -0800

    Added new multiple tracks feature
    Could be useful!

--- py/app.py
+++ py/app.py
@@ -18,17 +18,33 @@ def home():
 def hello(id):
 	id = id
 	id = "http://mp3skull.com/mp3/" + id + ".html"
+	#http://asian-central.com:8080/gorillaz%20feeling%20good
+	#http://asian-central.com:8080/
+	#source = html.parse("http://mp3skull.com/mp3/feel%20good.html")
 	
 	source = html.parse(id)
 	songs = source.xpath("//a[starts-with(text(),'Download')]/@href")
 	names = source.xpath("//a[starts-with(text(),'Download')]/text()")
 	
-	for i in songs:
-		#return '<audio><a href="' + i + '">' + i + '</a></audio>'
-		return """<audio autoplay='autoplay' controls='controls'>
-				  <source src='""" + i + """' type="audio/mpeg">
-				Your browser does not support the audio element.
-				</audio>"""
+	#return '<audio><a href="' + i + '">' + i + '</a></audio>'
+	
+	# for i in songs:
+		# print i
+		# return i
+	
+	return """<audio autoplay='autoplay' controls='controls'>
+			  <source src='""" + songs[1] + """' type="audio/mpeg">
+			Your browser does not support the audio element.
+			</audio>
+			<br><audio controls='controls'>
+			  <source src='""" + songs[2] + """' type="audio/mpeg">
+			Your browser does not support the audio element.
+			</audio>
+			<br><audio controls='controls'>
+			  <source src='""" + songs[3] + """' type="audio/mpeg">
+			Your browser does not support the audio element.
+			</audio>"""
+	
 	#return id
 
 
