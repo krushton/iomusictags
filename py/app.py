9f2cac931e2ac2a8715a4c779339351b7bde33f7
Peter Nguyen <ptt.nguyen@gmail.com>
2012-11-13 03:40:45 -0800

    Tested features, tested lxml for compatibility
    Just wanted to make sure things were working correctly before I went to
    bed.

--- py/app.py
+++ py/app.py
@@ -5,26 +5,29 @@ import urllib
 from lxml import etree
 from lxml import html
 
-
 # @route("/<url:path>")
 # def index(url):
   # return "Your url is " + url
 
 @route('/')
 def home():
-	return "Hello World!"
+	return "Welcome to Peter's Development Server!"
 
 @route('/<id>', method='GET')
 def hello(id):
 	id = id
-	id = "http://mp3skull.com/mp3/" + id + ".html"
+	id = "http://mp3skull.com/mp3/" + id.replace(" ","_") + ".html"
 	#http://asian-central.com:8080/gorillaz%20feeling%20good
 	#http://asian-central.com:8080/
+	
+	#from lxml import html
 	#source = html.parse("http://mp3skull.com/mp3/feel%20good.html")
 	
 	source = html.parse(id)
 	songs = source.xpath("//a[starts-with(text(),'Download')]/@href")
-	names = source.xpath("//a[starts-with(text(),'Download')]/text()")
+	#names = source.xpath("//a[starts-with(text(),'Download')]/text()")
+	song_names = source.xpath("//div[@id='right_song']/div/b/text()")
+	#song_info = source.xpath("//div[@class='left']/text()")
 	
 	#return '<audio><a href="' + i + '">' + i + '</a></audio>'
 	
@@ -32,18 +35,26 @@ def hello(id):
 		# print i
 		# return i
 	
+	song = songs[1]
+	# tested = str(exists(song))
+	
+	# if tested == False:
+		# song = songs[2]
+	
 	return """<audio autoplay='autoplay' controls='controls'>
-			  <source src='""" + songs[1] + """' type="audio/mpeg">
+			<source src='""" + song + """' type="audio/mpeg">
 			Your browser does not support the audio element.
 			</audio>
-			<br><audio controls='controls'>
-			  <source src='""" + songs[2] + """' type="audio/mpeg">
-			Your browser does not support the audio element.
-			</audio>
-			<br><audio controls='controls'>
-			  <source src='""" + songs[3] + """' type="audio/mpeg">
-			Your browser does not support the audio element.
-			</audio>"""
+			""" + "<div>" + song_names[1].replace("mp3","").title() + "<br></div>"
+			# + str(exists(songs[1]))+ str(exists(songs[2]))+ str(exists(songs[3]))
+			# <br><audio controls='controls'>
+			  # <source src='""" + songs[2] + """' type="audio/mpeg">
+			# Your browser does not support the audio element.
+			# </audio>
+			# <br><audio controls='controls'>
+			  # <source src='""" + songs[3] + """' type="audio/mpeg">
+			# Your browser does not support the audio element.
+			# </audio>
 	
 	#return id
 
